b9a17d1c86206653c56aea12cfec49d2f6154a29
Lyh <861407735@qq.com>
2021-08-05 21:34:11 +0800

    更改前端代码并覆盖

---
+++ src/main/resources/static/precache-manifest.8afb0171343670a956f6d00fc1e9639b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10356bc8798d18e9c35f9ca47ef21cb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa5c48a3e7d085337365",
+    "url": "./static/css/2.dab57b6f.chunk.css"
+  },
+  {
+    "revision": "d7cd49d9362fed3d1d88",
+    "url": "./static/css/main.a335d235.chunk.css"
+  },
+  {
+    "revision": "aa5c48a3e7d085337365",
+    "url": "./static/js/2.9429531b.chunk.js"
+  },
+  {
+    "revision": "17f0280f49c1b9a51cd8ae648e54dac7",
+    "url": "./static/js/2.9429531b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7cd49d9362fed3d1d88",
+    "url": "./static/js/main.73928e3a.chunk.js"
+  },
+  {
+    "revision": "2e943b1e63d9d7533b96",
+    "url": "./static/js/runtime-main.df84db3b.js"
+  },
+  {
+    "revision": "5df7c553cfd614176053331eb7b490cf",
+    "url": "./static/media/bg.5df7c553.webp"
+  }
+]);
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.800bf9e3c7dc1e188b14674be528cf14.js"
+  "./precache-manifest.8afb0171343670a956f6d00fc1e9639b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
